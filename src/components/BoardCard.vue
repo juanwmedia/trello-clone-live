b7818d82570e42261c379273cf2561da04e0eb8c
juanwmedia <juan@wmedia.es>
2021-06-29 15:01:19 +0200

    Destacar tarjetas completadas y caducadas

--- src/components/BoardCard.vue
+++ src/components/BoardCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     @click="openCard"
+    :class="{ done: card.done, overdue }"
     class="px-3 pt-3 pb-5 mb-3 | bg-white shadow rounded border border-white cursor-move"
   >
     <h4 class="text-gray-700 font-semibold font-sans tracking-wide text-md">
@@ -34,7 +35,11 @@ export default {
       openCard,
       cardDate: computed(() =>
         new Date(+props.card.date).toLocaleDateString("es-ES")
-      )
+      ),
+      overdue: computed(() => {
+        if (props.card.done) return false;
+        return props.card.date < Date.now();
+      })
     };
   },
   components: {
@@ -42,3 +47,14 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.done {
+  opacity: 0.5;
+  background-color: lightgreen;
+}
+
+.overdue {
+  background-color: tomato;
+}
+</style>
